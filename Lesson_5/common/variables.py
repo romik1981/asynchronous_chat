6ebbd30878ca1dad6775b9899723be1b90e8892a
Roman <belyakov1981@mail.ru>
2023-06-12 15:09:46 +0300

    Lesson_5 Ready

--- Lesson_5/common/variables.py
+++ Lesson_5/common/variables.py
@@ -1,7 +1,8 @@
 """Константы"""
 
+import logging
 
-# Порт по умолчанию для сетевого ваимодействия
+# Порт поумолчанию для сетевого ваимодействия
 DEFAULT_PORT = 7777
 # IP адрес по умолчанию для подключения клиента
 DEFAULT_IP_ADDRESS = '127.0.0.1'
@@ -11,8 +12,10 @@ MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_LEVEL = logging.DEBUG
 
-# Протокол JIM основные ключи:
+# Прококол JIM основные ключи:
 ACTION = 'action'
 TIME = 'time'
 USER = 'user'
@@ -22,7 +25,3 @@ ACCOUNT_NAME = 'account_name'
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
-
-
-def LOGGING_LEVEL():
-    return None
